15feff2f1699ff9858d0420d3ac701a338d126d4
itnextsolutions <itnextsolutions.web@gmail.com>
2023-02-23 17:07:26 +0530

    alpesh changes

--- VastraIndiaWebAPI/Models/ProductModel.cs
+++ VastraIndiaWebAPI/Models/ProductModel.cs
@@ -11,15 +11,21 @@ namespace VastraIndiaWebAPI.Models
         public int Product_Id { get; set; }
 
         public int Category_Id { get; set; }
+
         public int SubCategory_Id { get; set; }
+
         public int[] SizeId { get; set; }
 
         public int[] ColorId { get; set; }
+
         public string Product_Title { get; set; }
 
         public string Product_Description { get; set; }
 
         public string Image_Name { get; set; }
+
+        public IFormFile formFile { get; set; }
+
         //public int IsActive { get; set; }
 
         //public DateTime Created_Date { get; set; }
